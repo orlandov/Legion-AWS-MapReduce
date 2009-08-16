c4fa7c2c3e07fb6bc7d43cd4958e957d911e3b3c
Orlando Vazquez <ovazquez@gmail.com>
2009-08-15 23:38:59 -0700

    check in semi functional driver

--- slave/mapper.pl
+++ slave/mapper.pl
@@ -4,20 +4,43 @@ use strict;
 use warnings;
 
 use FindBin;
-use lib "$FindBin::Bin/slave-lib";
+use lib "$FindBin::Bin/slave-lib/lib/perl5";
+use local::lib "$FindBin::Bin/slave-lib";
 
 use Amazon::S3;
 
-
 use JSON;
+use YAML;
+
+my $s3;
+my $source;
+
+sub s3_init {
+    my $aws_conf = YAML::LoadFile('aws.yaml');
+
+    return if $s3;
+
+    $s3 = Amazon::S3->new(
+        $aws_conf->{hadoop_ec2}
+    );
+}
+
+sub source_init {
+    s3_init;
+    $bucket = $s3->bucket("source-legion-2wycked-net");
+    $bucket->get_key_filename($source, 'GET', $source);
+}
+
 while (<>) {
-    my $task = from_json($_);
     # read json
-    # check if blender cached
-    # fetch blender from s3 or cache
-    # cache blender
+    my $task = from_json($_);
+
     # read source file from s3 or cache
     # cache source file
+    source_init;
+
     # run blender
+    warn "Running blender";
+
     # push rendered frame to s3
 }
